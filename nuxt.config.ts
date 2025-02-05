5620169f62ed0eecdf15eb36d0d981da423d8461
eschoeDeveloper <dev.uschoe@gmail.com>
2025-02-05 13:59:58 +0900

    routerules add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: true,
   nitro: {
     preset: 'netlify',
+  },
+
+  routeRules: {
+    '/api/*': { prerender: true },
+    '/auth/*': { prerender: true },
   },
 
   typescript: {
