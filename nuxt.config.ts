ec21b0cec8a322f9bc03ccb9133c8b3665ab2572
eschoeDeveloper <dev.uschoe@gmail.com>
2025-02-05 21:49:21 +0900

    api call error debugging

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: true,
   nitro: {
-    preset: 'netlify',
+    preset: 'netlify-edge',
   },
   // routeRules: {
   //   '/api/*': { prerender: true },
